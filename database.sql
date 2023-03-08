a8abe961d201a53e17af29d4ce7ce43ea1636dd5
hnfrmdhni <91604141+hnfrmdhni@users.noreply.github.com>
2023-03-08 11:01:54 +0700

    Add table pesanan and bayar to database

--- database.sql
+++ database.sql
@@ -1,5 +1,5 @@
 CREATE TABLE user (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id_user INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL,
     nama VARCHAR(255) NOT NULL,
     alamat VARCHAR(255) NOT NULL,
@@ -10,11 +10,29 @@ CREATE TABLE user (
     rekening_bank VARCHAR(255) NULL
 );
 CREATE TABLE product (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    nama VARCHAR(255) NOT NULL,
+    id_produk INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nama_produk VARCHAR(255) NOT NULL,
     kategori VARCHAR(255) NOT NULL,
     deskripsi TEXT NOT NULL,
     harga INT NOT NULL,
     stok INT NOT NULL,
     berat FLOAT NOT NULL
+);
+CREATE TABLE pesanan (
+    id_pesanan INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id_user INT NOT NULL,
+    id_produk INT NOT NULL,
+    tgl_pesan DATE NOT NULL,
+    CONSTRAINT fk_user_pesanan
+    FOREIGN KEY (id_user) REFERENCES user(id_user),
+    CONSTRAINT fk_produk_pesanan
+    FOREIGN KEY (id_produk) REFERENCES product(id_produk)
+);
+CREATE TABLE bayar (
+    id_bayar INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id_pesanan INT NOT NULL,
+    tgl_bayar DATE NOT NULL,
+    total_bayar INT NOT NULL,
+    CONSTRAINT fk_bayar_pesanan
+    FOREIGN KEY (id_pesanan) REFERENCES bayar(id_pesanan)
 );
