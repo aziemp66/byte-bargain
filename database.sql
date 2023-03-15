3a9bdff420835e3921918b8da04a81894ce13c80
hnfrmdhni <91604141+hnfrmdhni@users.noreply.github.com>
2023-03-15 10:56:11 +0700

    revisi database

--- database.sql
+++ database.sql
@@ -1,57 +1,79 @@
-CREATE TABLE user (
-    id_user INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
+CREATE TABLE `user` (
+  `id_user` int PRIMARY KEY,
+  `email` VARCHAR(225) UNIQUE NOT NULL,
+  `password` VARCHAR(225) NOT NULL
 );
-CREATE TABLE pembeli (
-    id_pembeli INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    id_user INT NOT NULL,
-    nama VARCHAR(255) NOT NULL,
-    alamat VARCHAR(255) NOT NULL,
-    tgl_lahir DATE NOT NULL,
-    no_hp VARCHAR(20) NOT NULL,
-    jenis_kelamin ENUM('P', 'L') NOT NULL,
-    CONSTRAINT fk_user_pembeli
-    FOREIGN KEY (id_user) REFERENCES user(id_user),
+
+CREATE TABLE `pembeli` (
+  `id_pembeli` INT PRIMARY KEY,
+  `id_user` INT UNIQUE NOT NULL,
+  `nama` VARCHAR(225) NOT NULL,
+  `alamat` VARCHAR(225) NOT NULL,
+  `tgl_lahir` DATE NOT NULL,
+  `no_hp` VARCHAR(20) UNIQUE NOT NULL,
+  `jenis_kelamin` ENUM
 );
-CREATE TABLE penjual (
-    id_penjual INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    id_user INT NOT NULL,
-    nama VARCHAR(255) NOT NULL,
-    alamat VARCHAR(255) NOT NULL,
-    tgl_lahir DATE NOT NULL,
-    no_hp VARCHAR(20) NOT NULL,
-    jenis_kelamin ENUM('P', 'L') NOT NULL,
-    kartu_identitas VARCHAR(255) NULL,
-    rekening_bank VARCHAR(255) NULL,
-    CONSTRAINT fk_user_penjual
-    FOREIGN KEY (id_user) REFERENCES user(id_user),
+
+CREATE TABLE `penjual` (
+  `id_penjual` INT PRIMARY KEY,
+  `id_user` INT UNIQUE NOT NULL,
+  `nama` VARCHAR(225) NOT NULL,
+  `saldo` FLOAT NOT NULL,
+  `alamat` VARCHAR(225) NOT NULL,
+  `tgl_lahir` DATE NOT NULL,
+  `no_hp` VARCHAR(20) UNIQUE NOT NULL,
+  `jenis_kelamin` ENUM,
+  `kartu_identitas` VARCHAR(225) NOT NULL,
+  `rekening_bank` VARCHAR(225) NOT NULL,
+  `nomor_rekening` INT NOT NULL
 );
-CREATE TABLE produk (
-    id_produk INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    nama_produk VARCHAR(255) NOT NULL,
-    kategori VARCHAR(255) NOT NULL,
-    deskripsi TEXT NOT NULL,
-    harga INT NOT NULL,
-    stok INT NOT NULL,
-    berat FLOAT NOT NULL
+
+CREATE TABLE `produk` (
+  `id_produk` INT PRIMARY KEY,
+  `id_penjual` INT NOT NULL,
+  `nama_produk` VARCHAR(225) NOT NULL,
+  `kategori` VARCHAR(225) NOT NULL,
+  `deskripsi` TEXT NOT NULL,
+  `harga` FLOAT NOT NULL,
+  `stok` INT NOT NULL,
+  `berat` FLOAT NOT NULL
 );
-CREATE TABLE pesanan (
-    id_pesanan INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    id_user INT NOT NULL,
-    id_produk INT NOT NULL,
-    tgl_pesan DATE NOT NULL,
-    status VARCHAR(255) NOT NULL,
-    CONSTRAINT fk_user_pesanan
-    FOREIGN KEY (id_user) REFERENCES user(id_user),
-    CONSTRAINT fk_produk_pesanan
-    FOREIGN KEY (id_produk) REFERENCES product(id_produk)
+
+CREATE TABLE `pesanan` (
+  `id_pesanan` INT PRIMARY KEY,
+  `id_pembeli` INT NOT NULL,
+  `id_penjual` INT NOT NULL,
+  `waktu_transaksi` DATETIME NOT NULL,
+  `status` VARCHAR(225) NOT NULL
 );
-CREATE TABLE bayar (
-    id_bayar INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    id_pesanan INT NOT NULL,
-    tgl_bayar DATE NOT NULL,
-    total_bayar INT NOT NULL,
-    CONSTRAINT fk_bayar_pesanan
-    FOREIGN KEY (id_pesanan) REFERENCES pesanan(id_pesanan)
+
+CREATE TABLE `pesanan_produk` (
+  `id_pesanan_produk` INT PRIMARY KEY,
+  `id_produk` INT NOT NULL,
+  `id_pesanan` INT NOT NULL,
+  `qty` INT NOT NULL
 );
+
+CREATE TABLE `bayar` (
+  `id_bayar` INT PRIMARY KEY,
+  `id_pesanan` INT UNIQUE NOT NULL,
+  `tgl_bayar` DATE NOT NULL,
+  `total_bayar` FLOAT NOT NULL,
+  `metode_pembayaran` VARCHAR(20) NOT NULL
+);
+
+ALTER TABLE `pembeli` ADD FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`);
+
+ALTER TABLE `penjual` ADD FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`);
+
+ALTER TABLE `produk` ADD FOREIGN KEY (`id_penjual`) REFERENCES `penjual` (`id_penjual`);
+
+ALTER TABLE `pesanan` ADD FOREIGN KEY (`id_pembeli`) REFERENCES `pembeli` (`id_pembeli`);
+
+ALTER TABLE `pesanan` ADD FOREIGN KEY (`id_penjual`) REFERENCES `penjual` (`id_penjual`);
+
+ALTER TABLE `pesanan_produk` ADD FOREIGN KEY (`id_produk`) REFERENCES `produk` (`id_produk`);
+
+ALTER TABLE `pesanan_produk` ADD FOREIGN KEY (`id_pesanan`) REFERENCES `pesanan` (`id_pesanan`);
+
+ALTER TABLE `bayar` ADD FOREIGN KEY (`id_pesanan`) REFERENCES `pesanan` (`id_pesanan`);
