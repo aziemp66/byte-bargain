2087f0d1b515d19b10a5ba96060d92ea66ad1598
Azie Melza Pratama <aziemp66@gmail.com>
2023-04-02 12:18:54 +0700

    Creating User Domain

--- database.sql
+++ database.sql
@@ -1,33 +1,33 @@
 CREATE TABLE `user` (
-    `id_user` int PRIMARY KEY,
+    `id_user` VARCHAR(36) PRIMARY KEY,
     `email` VARCHAR(225) UNIQUE NOT NULL,
     `password` VARCHAR(225) NOT NULL
 );
 CREATE TABLE `pembeli` (
-    `id_pembeli` INT PRIMARY KEY,
-    `id_user` INT UNIQUE NOT NULL,
+    `id_pembeli` VARCHAR(36) PRIMARY KEY,
+    `id_user` VARCHAR(36) UNIQUE NOT NULL,
     `nama` VARCHAR(225) NOT NULL,
     `alamat` VARCHAR(225) NOT NULL,
     `tgl_lahir` DATE NOT NULL,
     `no_hp` VARCHAR(20) UNIQUE NOT NULL,
-    `jenis_kelamin` ENUM
+    `jenis_kelamin` ENUM('Laki-laki', 'Perempuan') NOT NULL
 );
 CREATE TABLE `penjual` (
-    `id_penjual` INT PRIMARY KEY,
-    `id_user` INT UNIQUE NOT NULL,
+    `id_penjual` VARCHAR(36) PRIMARY KEY,
+    `id_user` VARCHAR(36) UNIQUE NOT NULL,
     `nama` VARCHAR(225) NOT NULL,
     `saldo` FLOAT NOT NULL,
     `alamat` VARCHAR(225) NOT NULL,
     `tgl_lahir` DATE NOT NULL,
     `no_hp` VARCHAR(20) UNIQUE NOT NULL,
-    `jenis_kelamin` ENUM,
+    `jenis_kelamin` ENUM('Laki-laki', 'Perempuan') NOT NULL,
     `kartu_identitas` VARCHAR(225) NOT NULL,
     `rekening_bank` VARCHAR(225) NOT NULL,
     `nomor_rekening` INT NOT NULL
 );
 CREATE TABLE `produk` (
-    `id_produk` INT PRIMARY KEY,
-    `id_penjual` INT NOT NULL,
+    `id_produk` VARCHAR(36) PRIMARY KEY,
+    `id_penjual` VARCHAR(36) NOT NULL,
     `nama_produk` VARCHAR(225) NOT NULL,
     `kategori` VARCHAR(225) NOT NULL,
     `deskripsi` TEXT NOT NULL,
@@ -36,21 +36,21 @@ CREATE TABLE `produk` (
     `berat` FLOAT NOT NULL
 );
 CREATE TABLE `pesanan` (
-    `id_pesanan` INT PRIMARY KEY,
-    `id_pembeli` INT NOT NULL,
-    `id_penjual` INT NOT NULL,
+    `id_pesanan` VARCHAR(36) PRIMARY KEY,
+    `id_pembeli` VARCHAR(36) NOT NULL,
+    `id_penjual` VARCHAR(36) NOT NULL,
     `waktu_transaksi` DATETIME NOT NULL,
     `status` VARCHAR(225) NOT NULL
 );
 CREATE TABLE `pesanan_produk` (
-    `id_pesanan_produk` INT PRIMARY KEY,
-    `id_produk` INT NOT NULL,
-    `id_pesanan` INT NOT NULL,
+    `id_pesanan_produk` VARCHAR(36) PRIMARY KEY,
+    `id_produk` VARCHAR(36) NOT NULL,
+    `id_pesanan` VARCHAR(36) NOT NULL,
     `qty` INT NOT NULL
 );
 CREATE TABLE `bayar` (
-    `id_bayar` INT PRIMARY KEY,
-    `id_pesanan` INT UNIQUE NOT NULL,
+    `id_bayar` VARCHAR(36) PRIMARY KEY,
+    `id_pesanan` VARCHAR(36) UNIQUE NOT NULL,
     `tgl_bayar` DATE NOT NULL,
     `total_bayar` FLOAT NOT NULL,
     `metode_pembayaran` VARCHAR(20) NOT NULL
---
+++ internal/domain/user/type.go
@@ -0,0 +1,30 @@
+package user
+
+import "time"
+
+type (
+	User struct {
+		IdUser   string `json:"id_user"`
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+	Pembeli struct {
+		IdPembeli    string    `json:"id_pembeli"`
+		Nama         string    `json:"nama"`
+		Alamat       string    `json:"alamat"`
+		TanggalLahir time.Time `json:"tanggal_lahir"`
+		NoHp         string    `json:"no_hp"`
+		JenisKelamin string    `json:"jenis_kelamin"`
+	}
+	Penjual struct {
+		IdPenjual      string    `json:"id_penjual"`
+		Nama           string    `json:"nama"`
+		Alamat         string    `json:"alamat"`
+		TanggalLahir   time.Time `json:"tanggal_lahir"`
+		NoHp           string    `json:"no_hp"`
+		JenisKelamin   string    `json:"jenis_kelamin"`
+		KartuIdentitas string    `json:"kartu_identitas"`
+		RekeningBank   string    `json:"rekening_bank"`
+		NomoreRekening string    `json:"nomor_rekening"`
+	}
+)
