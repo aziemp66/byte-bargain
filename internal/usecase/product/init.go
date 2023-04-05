3bdc4c1e32cfd06fbfb607eaef6890a307acd07e
Azie Melza Pratama <aziemp66@gmail.com>
2023-04-05 21:47:01 +0700

    Creating Method for Usecase

--- internal/usecase/product/init.go
+++ internal/usecase/product/init.go
@@ -4,9 +4,104 @@ import (
 	"database/sql"
 
 	productRepository "github.com/aziemp66/byte-bargain/internal/repository/product"
+
+	httpCommon "github.com/aziemp66/byte-bargain/common/http"
+
+	"context"
 )
 
 type ProductUsecaseImplementation struct {
 	ProductRepository productRepository.Repository
 	DB                *sql.DB
+}
+
+func NewProductUsecaseImplementation(productRepository productRepository.Repository, db *sql.DB) *ProductUsecaseImplementation {
+	return &ProductUsecaseImplementation{
+		ProductRepository: productRepository,
+		DB:                db,
+	}
+}
+
+func (p *ProductUsecaseImplementation) GetRecommendedProduct(ctx context.Context) error {
+	return nil
+}
+
+func (p *ProductUsecaseImplementation) GetSearchedProduct(ctx context.Context, search string) (httpCommon.Product, error) {
+	return httpCommon.Product{}, nil
+}
+
+func (p *ProductUsecaseImplementation) GetAllProductBySellerID(ctx context.Context, sellerID string) ([]httpCommon.Product, error) {
+	return nil, nil
+}
+
+func (p *ProductUsecaseImplementation) GetProductByID(ctx context.Context, productID string) (httpCommon.Product, error) {
+	return httpCommon.Product{}, nil
+}
+
+func (p *ProductUsecaseImplementation) GetOrderByID(ctx context.Context, orderID string) error {
+	return nil
+}
+
+func (p *ProductUsecaseImplementation) GetCustomerOrderByID(ctx context.Context, customerID string) error {
+	return nil
+}
+
+func (p *ProductUsecaseImplementation) GetSellerOrderByID(ctx context.Context, sellerID string) error {
+	return nil
+}
+
+func (p *ProductUsecaseImplementation) GetOrderProductByID(ctx context.Context, orderProductID string) error {
+	return nil
+}
+
+func (p *ProductUsecaseImplementation) GetCustomerCart(ctx context.Context, customerID string) error {
+	return nil
+}
+
+func (p *ProductUsecaseImplementation) GetPaymentByID(ctx context.Context, paymentID string) error {
+	return nil
+}
+
+func (p *ProductUsecaseImplementation) InsertProduct(ctx context.Context, product httpCommon.Product) error {
+	return nil
+}
+
+func (p *ProductUsecaseImplementation) InsertOrder(ctx context.Context, createOrder httpCommon.CreateOrder) error {
+	return nil
+}
+
+func (p *ProductUsecaseImplementation) InsertOrderProduct(ctx context.Context, orderProduct httpCommon.OrderProduct) error {
+	return nil
+}
+
+func (p *ProductUsecaseImplementation) InsertCartProduct(ctx context.Context, cartProduct httpCommon.CartProduct) error {
+	return nil
+}
+
+func (p *ProductUsecaseImplementation) InsertPayment(ctx context.Context, payment httpCommon.Payment) error {
+	return nil
+}
+
+func (p *ProductUsecaseImplementation) UpdateProductByID(ctx context.Context, productID string, product httpCommon.Product) error {
+	return nil
+}
+
+func (p *ProductUsecaseImplementation) UpdateOrderStatusByID(ctx context.Context, orderID, status string) error {
+	return nil
+}
+
+func (p *ProductUsecaseImplementation) UpdateOrderProductQtyByID(ctx context.Context, orderProductID, quantity string) error {
+	return nil
+}
+
+func (p *ProductUsecaseImplementation) UpdateCartProductQtyByID(ctx context.Context, cartProductID, quantity string) error {
+	return nil
+}
+
+func (p *ProductUsecaseImplementation) DeleteProductByID(ctx context.Context, productID string) error {
+	return nil
+}
+
+func (p *ProductUsecaseImplementation) DeleteOrderProductByID(ctx context.Context, orderProductID string) error {
+	return nil
 }
--- internal/usecase/user/init.go
+++ internal/usecase/user/init.go
@@ -1,8 +1,10 @@
 package user
 
 import (
+	"context"
 	"database/sql"
 
+	httpCommon "github.com/aziemp66/byte-bargain/common/http"
 	sessionCommon "github.com/aziemp66/byte-bargain/common/session"
 
 	userRepository "github.com/aziemp66/byte-bargain/internal/repository/user"
@@ -22,6 +24,43 @@ func NewUserUsecaseImplementation(userRepository userRepository.Repository, db *
 	}
 }
 
-func (u *UserUsecaseImplementation) Login() {
+func (u *UserUsecaseImplementation) Login(ctx context.Context, login httpCommon.Login) error {
+	return nil
 
+}
+
+func (u *UserUsecaseImplementation) RegisterCustomer(ctx context.Context, registerCustomer httpCommon.RegisterCustomer) error {
+	return nil
+}
+
+func (u *UserUsecaseImplementation) RegisterSeller(ctx context.Context, registerSeller httpCommon.RegisterSeller) error {
+	return nil
+}
+
+func (u *UserUsecaseImplementation) GetCustomerByID(ctx context.Context, customerID string) (httpCommon.Customer, error) {
+	return httpCommon.Customer{}, nil
+}
+
+func (u *UserUsecaseImplementation) GetSellerByID(ctx context.Context, sellerID string) (httpCommon.Seller, error) {
+	return httpCommon.Seller{}, nil
+}
+
+func (u *UserUsecaseImplementation) ForgotPassword(ctx context.Context, forgotPassword httpCommon.ForgotPassword) error {
+	return nil
+}
+
+func (u *UserUsecaseImplementation) ResetPassword(ctx context.Context, id, token string) error {
+	return nil
+}
+
+func (u *UserUsecaseImplementation) ChangePassword(ctx context.Context, id string, ChangePassword httpCommon.ChangePassword) error {
+	return nil
+}
+
+func (u *UserUsecaseImplementation) UpdateCustomerByID(ctx context.Context, customer httpCommon.Customer) error {
+	return nil
+}
+
+func (u *UserUsecaseImplementation) UpdateSellerByID(ctx context.Context, seller httpCommon.Seller) error {
+	return nil
 }
