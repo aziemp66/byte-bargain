c7cb3333c20cd94ca8c9acd55f797c21022f34dc
Azie Melza Pratama <aziemp66@gmail.com>
2023-04-08 12:56:58 +0700

    Creating User Controller Skeleton

--- internal/controller/user/init.go
+++ internal/controller/user/init.go
@@ -1,1 +1,58 @@
 package user
+
+import (
+	userUseCase "github.com/aziemp66/byte-bargain/internal/usecase/user"
+	"github.com/gin-gonic/gin"
+)
+
+type UserController struct {
+	UserUsecase userUseCase.Usecase
+}
+
+func NewUserController(userUsecase userUseCase.Usecase) *UserController {
+	return &UserController{
+		UserUsecase: userUsecase,
+	}
+}
+
+func (u *UserController) Login(ctx *gin.Context) error {
+	return nil
+}
+
+func (u *UserController) RegisterCustomer(ctx *gin.Context) error {
+	return nil
+}
+
+func (u *UserController) RegisterSeller(ctx *gin.Context) error {
+	return nil
+}
+
+func (u *UserController) GetCustomerByID(ctx *gin.Context) error {
+	return nil
+}
+
+func (u *UserController) GetSellerByID(ctx *gin.Context) error {
+	return nil
+}
+
+func (u *UserController) ForgotPassword(ctx *gin.Context) error {
+	return nil
+}
+
+func (u *UserController) ResetPassword(ctx *gin.Context) error {
+	return nil
+}
+
+func (u *UserController) ChangePassword(ctx *gin.Context) error {
+	return nil
+}
+
+func (u *UserController) UpdateCustomerByID(ctx *gin.Context) error {
+	return nil
+}
+
+func (u *UserController) UpdateSellerByID(ctx *gin.Context) error {
+	return nil
+}
+
+// Path: internal/controller/user/init.go
