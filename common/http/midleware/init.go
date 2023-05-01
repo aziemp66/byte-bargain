dcc0e7a7e143deab44f79ea52ac958e2b36b7deb
Azie Melza Pratama <aziemp66@gmail.com>
2023-05-01 12:40:38 +0700

    Creating Session Auth Middleware

--- common/http/midleware/init.go
+++ common/http/midleware/init.go
@@ -1,1 +1,23 @@
 package midleware
+
+import (
+	sessionCommon "github.com/aziemp66/byte-bargain/common/session"
+	"github.com/gin-gonic/gin"
+)
+
+func SessionAuthMiddleware(sessionManager *sessionCommon.SessionManager) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		userID, ok := sessionManager.GetSessionValue(c, "user_id").(string)
+
+		if !ok || userID == "" {
+			c.AbortWithStatusJSON(401, gin.H{
+				"error": "Unauthorized",
+			})
+			return
+		}
+
+		c.Set("user_id", userID)
+
+		c.Next()
+	}
+}
